3feeef4e19473131b0d8e6a658174c08efb90fe8
JeffersonCarlosLima <jeffersoncl200@gmail.com>
2023-02-21 13:38:26 -0300

    commit

---
+++ src/script/script.js
@@ -0,0 +1,15 @@
+
+
+
+function calcular(){
+
+    let carboidratos = document.getElementById("carbo").value;
+    let proteinas = document.getElementById("protein").value;
+    let gordura = document.getElementById("fats").value;
+    
+    Resultado = ((gordura * 9)+(proteinas * 4)+(carboidratos * 4));
+
+    console.log(Resultado);
+}
+
+
