8e594849572c8cf21158e724719737317ef3386c
Jefferson Carlos Lima <92160349+JeffersonCarlosLima@users.noreply.github.com>
2023-03-01 14:44:25 -0300

    commit

--- src/script/script.js
+++ src/script/script.js
@@ -28,26 +28,26 @@ function calcularImc() {
     IMC = (peso / (altura*altura));
     document.getElementById("resultado-imc").innerHTML=IMC;
 
-    if (IMC <= 18,5){
-    document.getElementById("orientacao-imc").innerHTML="Resultado menor que 18,5 - Abaixo do peso";
+    if (IMC <= 18.5){
+    document.getElementById("orientacao-imc").innerHTML="Resultado menor que 18.5 - Abaixo do peso";
     }
-    else if(IMC>= 18,5 <= 24,9){
-        document.getElementById("orientacao-imc").innerHTML="18,5-24,9 - Normal";
+    else if(IMC>= 18.5 <= 24.9){
+        document.getElementById("orientacao-imc").innerHTML="18.5-24.9 - Normal";
      }   
-    else if(IMC>= 25 <= 29,9){
-        document.getElementById("orientacao-imc").innerHTML="25,0-29,9 - Excesso de peso";
+    else if(IMC>= 25 <= 29.9){
+        document.getElementById("orientacao-imc").innerHTML="25.0-29.9 - Excesso de peso";
      }     
-     else if(IMC>= 25 <= 29,9){
-        document.getElementById("orientacao-imc").innerHTML="25,0-29,9 - Excesso de peso";
+     else if(IMC>= 25 <= 29.9){
+        document.getElementById("orientacao-imc").innerHTML="25.0-29.9 - Excesso de peso";
      }
-     else if(IMC>= 30 <= 34,9){
-        document.getElementById("orientacao-imc").innerHTML="30,0-34,9 - Obesidade Leve (Grau I)";
+     else if(IMC>= 30 <= 34.9){
+        document.getElementById("orientacao-imc").innerHTML="30.0-34.9 - Obesidade Leve (Grau I)";
      }
-     else if(IMC>= 35 <= 39,9){
-        document.getElementById("orientacao-imc").innerHTML="35,0-39,9 - Obesidade Severa (Grau II)";
+     else if(IMC>= 35 <= 39.9){
+        document.getElementById("orientacao-imc").innerHTML="35.0-39.9 - Obesidade Severa (Grau II)";
      }
      else if(IMC>= 40){
-        document.getElementById("orientacao-imc").innerHTML="> 40,0 - Obesidade Mórbida (Grau III)";
+        document.getElementById("orientacao-imc").innerHTML="> 40.0 - Obesidade Mórbida (Grau III)";
      }
    
 }
